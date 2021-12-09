ff33213b1920bca952acb6ef5cd2bf155a249333
luisresende13 <luisresende13@gmail.com>
2021-12-09 13:25:52 -0300

    HTTPS

--- app.js
+++ app.js
@@ -41,6 +41,20 @@ connect.then((db) => {
 
 var app = express();
 
+/*  ------------------- Security management ------------- */
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+  	console.log('Redirecting Server...')
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
